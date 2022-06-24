8e3418d47068e0c5c1001d8a84bac00308e352a0
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-24 17:02:46 +0200

    feat(front): implment AddProjectModal

---
+++ client/src/components/AddProjectModal.jsx
@@ -0,0 +1,158 @@
+import { useState } from 'react'
+import { FaList } from 'react-icons/fa'
+import { useMutation, useQuery } from '@apollo/client'
+import { ADD_PROJECT } from '../mutations/projectMutation'
+import { GET_PROJECTS } from '../queries/projectQueries'
+import { GET_CLIENTS } from '../queries/clientQueries'
+
+const AddProjectModal = () => {
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [clientId, setClientId] = useState('')
+  const [status, setStatus] = useState('new')
+
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, clientId, status },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS })
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject] },
+      })
+    },
+  })
+
+  const { loading, error, data } = useQuery(GET_CLIENTS)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (name === '' || description === '' || status === '' || clientId === '') {
+      return alert('Please fill in all fields')
+    }
+    addProject(name, description, status, clientId)
+
+    setName('')
+    setDescription('')
+    setStatus('new')
+    setClientId('')
+  }
+
+  if (loading) return null
+  if (error) return <p>Something Went Wrong</p>
+
+  return (
+    <>
+      {!loading && !error && (
+        <>
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#addProjectModal"
+          >
+            <div className="d-flex align-items-center">
+              <FaList className="icon" />
+              <div>New Project</div>
+            </div>
+          </button>
+
+          <div
+            className="modal fade"
+            id="addProjectModal"
+            aria-labelledby="addProjectModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="addProjectModalLabel">
+                    New Project
+                  </h5>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <form onSubmit={onSubmit}>
+                    <div className="mb-3">
+                      <label className="form-label" htmlFor="name">
+                        Name
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label" htmlFor="description">
+                        Description
+                      </label>
+                      <textarea
+                        className="form-control"
+                        id="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                      ></textarea>
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label" htmlFor="status">
+                        Status
+                      </label>
+                      <select
+                        name="status"
+                        id="status"
+                        className="form-select"
+                        value={status}
+                        onChange={(e) => setStatus(e.target.value)}
+                      >
+                        <option value="new">Not Started</option>
+                        <option value="progess">In Progress</option>
+                        <option value="completed">Completed</option>
+                      </select>
+                    </div>
+
+                    <div className="mb-3">
+                      <label className="form-label" htmlFor="clientId">
+                        Client
+                      </label>
+                      <select
+                        name="clientId"
+                        id="clientId"
+                        className="form-select"
+                        value={clientId}
+                        onChange={(e) => setClientId(e.target.value)}
+                      >
+                        <option value="">---</option>
+                        {data.clients.map((client) => (
+                          <option key={client.id} value={client.id}>
+                            {client.name}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+
+                    <button
+                      type="submit"
+                      data-bs-dismiss="modal"
+                      className="btn btn-primary"
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  )
+}
+
+export default AddProjectModal
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import Clients from '../components/Clients'
 import AddClientModal from '../components/AddClientModal'
+import AddProjectModal from '../components/AddProjectModal'
 import Projects from '../components/Projects'
+import Clients from '../components/Clients'
 
 const Home = () => (
-    <>
-        <div className="d-flex gap-3 mb-4">
-            <AddClientModal />
-        </div>
-        <Projects />
-        <hr />
-        <Clients />
-    </>
+  <>
+    <div className="d-flex gap-3 mb-4">
+      <AddClientModal />
+      <AddProjectModal />
+    </div>
+    <Projects />
+    <hr />
+    <Clients />
+  </>
 )
 
 export default Home
